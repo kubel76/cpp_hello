72ceb007f84874652d52bd2f95d60582a9d1d2d6
Petr Burchak <pb@mail.com>
2026-01-20 21:27:36 +0200

    variable

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
+#include<string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int age = 25;
+    double height = 1.72;
+    char gender = 'M';
+    bool isStudent = true;
+    std::string name = "Peter";
+
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "Age: " << age << std::endl;
+    std::cout << "Height: " << height << " meters" << std::endl;
+    std::cout << "Gender: " << gender << std::endl;
+    std::cout << "Is Student: " << isStudent << std::endl;
+
     return 0;
 }
